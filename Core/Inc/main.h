c4bf0eb5366f039d514ef39e015c1ec1b64c9660
ShyngysM <tschingis.muratbek@gmail.com>
2024-02-09 17:11:00 +0100

    Copy of working brach from this repo: https://github.com/ShyngysM/stm32_multimodul.git

---
+++ Core/Inc/main.h
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define OSC32_IN_Pin GPIO_PIN_14
+#define OSC32_IN_GPIO_Port GPIOC
+#define OSC32_OUT_Pin GPIO_PIN_15
+#define OSC32_OUT_GPIO_Port GPIOC
+#define USB_FS_PWR_EN_Pin GPIO_PIN_10
+#define USB_FS_PWR_EN_GPIO_Port GPIOF
+#define PH0_MCU_Pin GPIO_PIN_0
+#define PH0_MCU_GPIO_Port GPIOH
+#define PH1_MCU_Pin GPIO_PIN_1
+#define PH1_MCU_GPIO_Port GPIOH
+#define ANALOG_READ_PA3_Pin_Pin GPIO_PIN_3
+#define ANALOG_READ_PA3_Pin_GPIO_Port GPIOA
+#define PUMP_PA5_Pin_Pin GPIO_PIN_5
+#define PUMP_PA5_Pin_GPIO_Port GPIOA
+#define LD1_Pin GPIO_PIN_0
+#define LD1_GPIO_Port GPIOB
+#define STEPPER3_PE13_Pin_Pin GPIO_PIN_13
+#define STEPPER3_PE13_Pin_GPIO_Port GPIOE
+#define STEPPER4_PE14_Pin_Pin GPIO_PIN_14
+#define STEPPER4_PE14_Pin_GPIO_Port GPIOE
+#define LD3_Pin GPIO_PIN_14
+#define LD3_GPIO_Port GPIOB
+#define STLINK_RX_Pin GPIO_PIN_8
+#define STLINK_RX_GPIO_Port GPIOD
+#define STLINK_TX_Pin GPIO_PIN_9
+#define STLINK_TX_GPIO_Port GPIOD
+#define USB_FS_OVCR_Pin GPIO_PIN_7
+#define USB_FS_OVCR_GPIO_Port GPIOG
+#define USB_FS_VBUS_Pin GPIO_PIN_9
+#define USB_FS_VBUS_GPIO_Port GPIOA
+#define USB_FS_ID_Pin GPIO_PIN_10
+#define USB_FS_ID_GPIO_Port GPIOA
+#define USB_FS_N_Pin GPIO_PIN_11
+#define USB_FS_N_GPIO_Port GPIOA
+#define USB_FS_P_Pin GPIO_PIN_12
+#define USB_FS_P_GPIO_Port GPIOA
+#define HELP_SIGNAL_PG9_Pin_Pin GPIO_PIN_9
+#define HELP_SIGNAL_PG9_Pin_GPIO_Port GPIOG
+#define VIBROMOTOR_PG12_Pin_Pin GPIO_PIN_12
+#define VIBROMOTOR_PG12_Pin_GPIO_Port GPIOG
+#define STEPPER2_PG14_Pin_Pin GPIO_PIN_14
+#define STEPPER2_PG14_Pin_GPIO_Port GPIOG
+#define STEPPER1_PB6_Pin_Pin GPIO_PIN_6
+#define STEPPER1_PB6_Pin_GPIO_Port GPIOB
+#define LD2_Pin GPIO_PIN_1
+#define LD2_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
