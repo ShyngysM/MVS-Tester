6b358f662ddd853a43d1b857fcc023a4eee2e1d7
ShyngysM <tschingis.muratbek@gmail.com>
2024-07-16 13:01:59 +0200

    saving the progres, rotate.c works but -number = clockwise

--- Core/Src/rotate.c
+++ Core/Src/rotate.c
@@ -2,12 +2,12 @@
 
 void rotate(int degree, int enc_position){
   if (enc_position > degree ) {
-   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
-  }
-  if (enc_position < degree ) {
    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+  }
+  if (enc_position < degree ) {
+   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
   }
   if (enc_position == degree ) {
    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
